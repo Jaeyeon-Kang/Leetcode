fefc9a458c29132f75aed7227ae3e2421586f33d
Kang Jaeyeon <93374962+Jaeyeon-Kang@users.noreply.github.com>
2024-05-31 21:29:56 +0900

    Time: 112 ms (5.06%), Space: 58 MB (5.03%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let answer=0;
+    for(let i=0; i<s.length-1; i++){
+    answer += (Math.abs(s[i].charCodeAt()-s[i+1].charCodeAt()))
+      console.log(answer)
+    }
+    return answer
+};
