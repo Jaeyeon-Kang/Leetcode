49a3ac5d96a06277925fc56319a2eef5c0a99d0f
Kang Jaeyeon <93374962+Jaeyeon-Kang@users.noreply.github.com>
2024-10-25 16:10:58 +0900

    Time: 0 ms (100.00%), Space: 52 MB (10.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,20 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let lt=0;
-    let rt=lt+1
-    let answer=[]
- 
-    while(lt<nums.length-1){
-        if((nums[lt]+nums[rt]) === target){
-            answer.push(lt)
-            answer.push(rt)
-            return answer;
-        } else if (rt < nums.length-1){
-            rt++
-        } else if(rt === nums.length-1){
-            lt++
-            rt=lt+1
+   let numMap = new Map()
+  
+    for(let i=0; i<nums.length; i++){
+        numMap.set(nums[i], i)
+    }
+    
+    for(let i=0; i<nums.length; i++){
+        const complement = target-nums[i]
+        if(numMap.has(complement) && numMap.get(complement) !== i){
+            return [i, numMap.get(complement)]
         }
     }
+    console.log(numMap)
 };
